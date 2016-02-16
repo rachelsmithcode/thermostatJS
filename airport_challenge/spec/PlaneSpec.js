5e766da80c65c58afa4b02ee9ebedddc61729f3a
Michael Collins <michaelj.collinsuk@gmail.com>
2016-02-16 14:42:34 +0000

    Create spec and src files

--- airport_challenge/spec/PlaneSpec.js
+++ airport_challenge/spec/PlaneSpec.js
@@ -1,12 +1,1 @@
 'use strict';
-
-describe('Plane', function() {
-  var plane;
-  beforeEach(function(){
-    plane = new Plane();
-  });
-
-  it('can land at an airport', function() {
-    expect(plane.land).not.toBeUndefined;
-  });
-});
