7749b9716d671697e40e95f815ac0b079f928719
Marion LV <marion.levisage@gmail.com>
2016-02-16 15:27:06 +0000

    TDDed first user story, created feature and unit tests that all pass

--- airport_challenge/src/Airport.js
+++ airport_challenge/src/Airport.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function Airport(){
+  this._hangar = [];
+}
+Airport.prototype.planes = function(){
+  return this._hangar;
+};
+
+Airport.prototype.clearForLanding = function(plane){
+  this._hangar.push(plane);
+};
